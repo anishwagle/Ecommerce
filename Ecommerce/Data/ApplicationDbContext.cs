02cab0fd29bd7ef9d020899e60c6653516519e83
Anish Wagle <anish.wagle192@gmail.com>
2023-04-19 07:36:47 +0545

    added carousel view and setup layout page

--- Ecommerce/Data/ApplicationDbContext.cs
+++ Ecommerce/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Ecommerce.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Ecommerce.Data
@@ -9,5 +10,14 @@ namespace Ecommerce.Data
             : base(options)
         {
         }
+
+        public DbSet<Carousel> Carousels { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<CartProductHelper> CartProductHelpers { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Image> Images { get; set; }
+        public DbSet<ImageProductHelper> ImageProductHelpers { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategoryHelper> ProductCategoryHelpers { get; set; }
     }
 }
