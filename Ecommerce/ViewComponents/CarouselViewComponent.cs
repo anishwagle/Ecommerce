f6ca3b16a46c0c1f2c0ddfbd6dc9a2cd807523e1
suresh dangol <surace997@gmail.com>
2023-04-20 09:52:02 -0500

    added carouselviewcomponent

---
+++ Ecommerce/ViewComponents/CarouselViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Ecommerce.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace Ecommerce.ViewComponents
+{
+    public class CarouselViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _context;
+        public CarouselViewComponent(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var carousels = _context.Carousels.Include(x => x.Image);
+            return View(carousels);
+        }
+
+    }
+}
